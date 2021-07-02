b636ffdeb4b297538869f9d77c5bfb7ce701459c
Pavan <pavans.shetty.2452@gmail.com>
2021-07-03 00:38:18 +0530

    updated react code to es7
    Co-authored-by: Manjunath G

--- App.js
+++ App.js
@@ -1,103 +1,74 @@
-import React, { Component } from 'react';
+import React , {useState} from 'react';
 import { AppRegistry, View, Text, StyleSheet, Platform } from 'react-native';
+import { Header } from 'react-native-elements';
+import ""
 
 import Name from './components/time'
+const App = () => {
+    let newTime;
+    let newAmPm;
+    let newDay;
+    
+const [time, setTime] = useState();
+const [AmPm, setAmPm] = useState();
+const [day, setDayy] = useState();
 
-export default class App extends Component {
 
-  constructor() {
-    super();
-
-    this.state = { currentTime: null, currentDay: null }
-    this.daysArray = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
-  }
-
-  componentWillMount() {
-    this.getCurrentTime();
-  }
-
-  getCurrentTime = () => {
-    let hour = new Date().getHours();
-    let minutes = new Date().getMinutes();
-    let seconds = new Date().getSeconds();
-    let am_pm = 'pm';
-
-    if (minutes < 10) {
-      minutes = '0' + minutes;
-    }
-
-    if (seconds < 10) {
-      seconds = '0' + seconds;
-    }
-
-    if (hour > 12) {
-      hour = hour - 12;
-    }
-
-    if (hour == 0) {
-      hour = 12;
-    }
-
-    if (new Date().getHours() < 12) {
-      am_pm = 'am';
-    }
-
-    this.setState({ currentTime: hour + ':' + minutes + ':' + seconds + ' ' + am_pm });
-
-    this.daysArray.map((item, key) => {
-      if (key == new Date().getDay()) {
-        this.setState({ currentDay: item.toUpperCase() });
-      }
-    })
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.timer);
-  }
-
-  componentDidMount() {
-    this.timer = setInterval(() => {
-      this.getCurrentTime();
-    }, 1000);
-  }
-
-  render() {
-
-    return (
-      <View style={styles.container}>
-        <View>
-          <Text style={styles.daysText}>{this.state.currentDay}</Text>
-          <Text style={styles.timeText}>{this.state.currentTime}</Text>
-          <Name />
-        </View>
-      </View>
-    );
-  }
+const updateTime = () => {
+    newTime = new Date().toLocaleTimeString();
+    setTime(newTime);
+    newAmPm = new Date().getHours();
+    setAmPm(newAmPm);
+    newDay = new Date().getDay();
+    setDayy(newDay);
 }
 
-const styles = StyleSheet.create(
-  {
-    container: {
-      flex: 1,
-      paddingTop: (Platform.OS === 'ios') ? 20 : 0,
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-    headerText: {
-      fontSize: 68,
-      textAlign: "center",
-      margin: 10,
-      color: 'black',
-      fontWeight: "bold"
-    },
-    timeText: {
-      fontSize: 50,
-      color: '#f44336'
-    },
-    daysText: {
-      color: '#2196f3',
-      fontSize: 25,
-      paddingBottom: 10
-    }
+setInterval(updateTime,1000)
+    return (
+        <>
+        <View >
+        <Header
+            centerComponent={{ text: 'Digital Clock', style: { color: '#fff',fontSize:15,fontWeight: 'bold'} }}
+       / >
+       {
+           (AmPm < 12)? 
+           <Text>am</Text>
+           :
+           
+           <Text>pm</Text>
+       }
 
-  });
+            <Text className="text">{time}</Text>
+            <Text>
+                {
+                    (day === 1)? 
+                    <Text>Monday</Text>
+                    :
+                    (day === 2)? 
+                    <Text>Tue</Text>
+                    :
+                    (day === 3)? 
+                    <Text>wedday</Text>
+                    :
+                    (day === 4)? 
+                    <Text>thuday</Text>
+                    :
+                    (day === 5)? 
+                    <Text>friday</Text>
+                    :
+                    (day === 6)? 
+                    <Text>satday</Text>
+                    :
+                    <Text>sunday</Text>
+
+
+                }
+            </Text>
+
+    </View>
+        </>
+    )
+}
+
+export default App
+
--- components/time.js
+++ components/time.js
@@ -4,10 +4,20 @@ import { AppRegistry, View, Text, StyleSheet, Platform } from 'react-native';
 function Name() {
     return(
         
-     <Text>Pavan Shetty</Text>
+     <Text style={styles.myName}>Pavan Shetty</Text>
      
     );
     
+    
 }
+const styles = StyleSheet.create(
+  {
+    myName: {
+     color:"#ffa500",
+    
+    },
+    
+
+  });
 
 export default Name
