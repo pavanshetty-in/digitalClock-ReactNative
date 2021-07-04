8b4d5c269596639f824564c4f9cc2af0a71a5dd0
Pavan <pavans.shetty.2452@gmail.com>
2021-07-04 23:22:46 +0530

    add fonts

--- App.js
+++ App.js
@@ -1,74 +1,148 @@
-import React , {useState} from 'react';
-import { AppRegistry, View, Text, StyleSheet, Platform } from 'react-native';
-import { Header } from 'react-native-elements';
-import ""
+import React, { useState } from "react";
+import {
+  AppRegistry,
+  View,
+  StyleSheet,
+  Platform,
+  Text,
+  ImageBackground,
+} from "react-native";
+import { Header } from "react-native-elements";
 
-import Name from './components/time'
+// import {
+//   useFonts,
+//  POPPINS_400Regular,
+//  POPPINS_500Regular,
+//  POPPINS_200Regular,
+//  POPPINS_600Regular,
+
+// } from "@expo-google-fonts/POPPINS";
+
+import Name from "./components/time";
 const App = () => {
-    let newTime;
-    let newAmPm;
-    let newDay;
-    
-const [time, setTime] = useState();
-const [AmPm, setAmPm] = useState();
-const [day, setDayy] = useState();
+  let newTime;
+  let hours;
+  let minutes;
+  let seconds;
+  let newDay;
 
+  const [time, setTime] = useState();
+  const [hour, setHour] = useState();
+  const [minute, setMinutes] = useState();
+  const [second, setSeconds] = useState();
+  const [day, setDayy] = useState();
 
-const updateTime = () => {
+  const updateTime = () => {
     newTime = new Date().toLocaleTimeString();
     setTime(newTime);
-    newAmPm = new Date().getHours();
-    setAmPm(newAmPm);
+
+    hours = new Date().getHours();
+
+    setHour(hours);
+
+    minutes = new Date().getMinutes();
+    setMinutes(minutes);
+    seconds = new Date().getSeconds();
+    setSeconds(seconds);
+
     newDay = new Date().getDay();
     setDayy(newDay);
-}
+  };
 
-setInterval(updateTime,1000)
-    return (
-        <>
-        <View >
+  setInterval(updateTime, 1000);
+  let bg1 = {
+    uri: "https://firebasestorage.googleapis.com/v0/b/digital-clock-1f6ae.appspot.com/o/bg1.jpg?alt=media&token=65c14ea1-0a21-4adb-be23-1cc74eeb7945",
+  };
+  if (hour > 20) {
+    bg1 = {
+      uri: "https://firebasestorage.googleapis.com/v0/b/digital-clock-1f6ae.appspot.com/o/bg4.jpg?alt=media&token=8961818f-fc6a-4c76-9e73-d06ce9aaaa07",
+    };
+  }
+  return (
+    <>
+      {/* <View>
         <Header
-            centerComponent={{ text: 'Digital Clock', style: { color: '#fff',fontSize:15,fontWeight: 'bold'} }}
-       / >
-       {
-           (AmPm < 12)? 
-           <Text>am</Text>
-           :
-           
-           <Text>pm</Text>
-       }
+          centerComponent={{
+            text: "Digital Clock",
+            style: { color: "#fff", fontSize: 15, fontWeight: "bold" },
+          }}
+        />
+        <View style={styles.timesec}>
+          <Text style={styles.timetxt}>
+            {hour > 12 ? hour - 12 : hour == 0 ? 12 : hour}:
+            {minute < 10 ? "0" + minute : minute}
+          </Text>
+          <Text style={styles.timeam}>{hour < 12 ? "AM" : "PM"}</Text>
+        </View>
 
-            <Text className="text">{time}</Text>
-            <Text>
-                {
-                    (day === 1)? 
-                    <Text>Monday</Text>
-                    :
-                    (day === 2)? 
-                    <Text>Tue</Text>
-                    :
-                    (day === 3)? 
-                    <Text>wedday</Text>
-                    :
-                    (day === 4)? 
-                    <Text>thuday</Text>
-                    :
-                    (day === 5)? 
-                    <Text>friday</Text>
-                    :
-                    (day === 6)? 
-                    <Text>satday</Text>
-                    :
-                    <Text>sunday</Text>
-
-
-                }
+        <Text>{second < 10 ? "0" + second : second}</Text>
+        <Text>
+          {day === 1
+            ? "Monday"
+            : day === 2
+            ? "Tuesday"
+            : day === 3
+            ? "Wednesday"
+            : day === 4
+            ? "Thursday"
+            : day === 5
+            ? "Friday"
+            : day === 6
+            ? "Saturday"
+            : "Sunday"}
+        </Text>
+      </View> */}
+      <View style={styles.page}>
+        <ImageBackground source={bg1} style={styles.bg}>
+          <View>
+            <Text style={styles.head}>Digital Clock</Text>
+            {/* <Text style={styles.timetxt}>{hour}</Text> */}
+          </View>
+          <View>
+            <Text style={styles.timetxt}>
+              {hour > 12 ? hour - 12 : hour == 0 ? 12 : hour}:
+              {minute < 10 ? "0" + minute : minute}
             </Text>
+          </View>
+          <View>
+            <Text>Pavan S</Text>
+          </View>
+        </ImageBackground>
+      </View>
+    </>
+  );
+};
+const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+    height: "100%",
+    width: "100%",
+  },
+  bg: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "space-between",
+  },
+  head: {
+    fontSize: 38,
+    paddingTop: "15%",
+    paddingLeft: 20,
+    color: "#ffffff",
+    
+  },
 
-    </View>
-        </>
-    )
-}
+  timetxt: {
+    fontSize: 68,
+    color: "#ffffff",
+    marginBottom: "80%",
+    paddingLeft: 20,
+    fontWeight:'100',
+  },
+  timeam: {
+    alignSelf: "baseline",
+    padding: 10,
+    fontSize: 28,
+  },
+});
 
-export default App
-
+export default App;
